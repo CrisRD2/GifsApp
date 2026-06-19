2868199c85b7ad7b669afa22c90a762eca3a72f2
CrisRD2 <crruizdelgado@gmail.com>
2026-06-19 12:20:16 +0200

    Creaccion de componentes necesarios, para una mejor optimizacion del codigo

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { mockGifs } from './mock-data/gifs.mock'
 import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
-import { PreviousSearches } from './gifs/PreviousSearches'
+import { PreviousSearches } from './gifs/components/PreviousSearches'
+import { GifsList } from './gifs/components/GifsList'
 
 export const GifsApp = () => {
     return (
@@ -10,24 +11,11 @@ export const GifsApp = () => {
             {/* Header */}
             <CustomHeader title='Buscador de gifs' description='Descubre y comparte el gif perfecto'></CustomHeader>
             {/* Search */}
-            <SearchBar />
+            <SearchBar  placeholder="Buscar gifs" />
             {/* Busquedas previas */}
            <PreviousSearches/>
             {/* Gifs */}
-            <div className='gifs-container'>
-
-                {
-                    mockGifs.map((gif) => (
-                        <div key={gif.id} className='gif-card'>
-                            <img src={gif.url} alt={gif.title} />
-                            <h3>{gif.title}</h3>
-                            <p>
-                                {gif.width}x{gif.height}(1.5mb)
-                            </p>
-                        </div>
-                    ))
-                }
-            </div>
+            <GifsList gifs={mockGifs}/>
         </>
 
     )
--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const SearchBar = () => {
+interface Props{
+    placeholder:string;
+}
+export const SearchBar = ({placeholder}:Props) => {
     return (
         <div className='search-container'>
-            <input type="text" placeholder='Buscar gifs' />
+            <input type="text" placeholder={placeholder} />
             <button>Buscar</button>
         </div>
     )
