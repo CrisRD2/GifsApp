bc59e7f0a65748c381796a98587bae405d1fc084
CrisRD2 <crruizdelgado@gmail.com>
2026-06-30 12:25:06 +0200

    Evitando repeticiones de peticion http, al usar searchPrevious

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,37 +1,11 @@
-import { useState } from 'react'
-import { mockGifs } from './mock-data/gifs.mock'
 import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
 import { PreviousSearches } from './gifs/components/PreviousSearches'
 import { GifsList } from './gifs/components/GifsList'
-import { getGifByQuery } from './gifs/actions/get-gifs-by-query.action'
-import type { Gif } from './gifs/interfaces/gif.interface';
+import { useGifs } from './gifs/hooks/useGifs'
 export const GifsApp = () => {
-    const [gifs, setGifs] = useState<Gif[]>([]);
-    const [previousTerms, setPreviousTerms] = useState<string[]>([])
-    const handleTermClick = (term: string) => {
-        handleSearch(term, true);
-    };
-    const handleSearch = async (query: string = '', isFromHistory = false) => {
 
-        const term = query.trim().toLowerCase();
-        if (term.length === 0) return;
-
-        if (previousTerms.includes(term) && !isFromHistory) return;
-
-        // COn esto evitamos que nos de un previous con mas de 8 elementos
-        setPreviousTerms((prev) => {
-            const filtered = prev.filter(item => item !== term);
-            return [term, ...filtered].slice(0, 8);
-        });
-
-
-
-        const gifs = await getGifByQuery(query);
-        console.log({ gifs });
-        setGifs(gifs);
-
-    }
+    const { gifs, previousTerms, handleSearch, handleTermClick } = useGifs();
 
     return (
         <>
---
+++ src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from 'react'
+import { getGifByQuery } from '../actions/get-gifs-by-query.action';
+import type { Gif } from '../interfaces/gif.interface';
+
+export const useGifs = () => {
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([])
+     const gifsCache = useRef<Record<string, Gif[]>>({});
+
+
+    const handleTermClick = async (term: string) => {
+        if (gifsCache.current[term]) {
+            return;
+        }
+        const gifs = await getGifByQuery(term);
+        setGifs(gifs);
+    };
+    const handleSearch = async (query: string = '') => {
+
+        query.trim().toLowerCase();
+        if (query.length === 0) return;
+
+        if (previousTerms.includes(query)) return;
+        setPreviousTerms([query, ...previousTerms].splice(0, 8));
+
+        // COn esto evitamos que nos de un previous con mas de 8 elementos
+        /* setPreviousTerms((prev) => {
+            const filtered = prev.filter(item => item !== term);
+            return [term, ...filtered].slice(0, 8);
+        });
+ */
+
+
+        const gifs = await getGifByQuery(query);
+
+        setGifs(gifs);
+        gifsCache.current[query] = gifs;
+
+    }
+
+    return {
+        // values
+        gifs,
+        previousTerms,
+
+        //Methods / actions
+        handleTermClick,
+        handleSearch
+
+
+    }
+
+
+}
